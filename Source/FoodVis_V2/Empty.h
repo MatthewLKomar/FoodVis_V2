d7e21f63ab7eb80f109da0345cafc96b54c9a0df
Streakman <46490210+MatthewLKomar@users.noreply.github.com>
2021-11-30 14:09:09 -0500

    push cpp

---
+++ Source/FoodVis_V2/Empty.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class FOODVIS_V2_API Empty
+{
+public:
+	Empty();
+	~Empty();
+};
---
+++ Source/FoodVis_V2/FoodVis_V2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FoodVis_V2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FoodVis_V2, "FoodVis_V2" );
